b94ff49035e9d0d4ee745a728facfd22b1adde5c
Lubos Lehota <lubos.lehota@gmail.com>
2025-07-25 12:02:15 +0200

    Add project

---
+++ etnetera-task/etnetera-task/App/View/AppView.swift
@@ -0,0 +1,17 @@
+//
+//  etnetera_taskApp.swift
+//  etnetera-task
+//
+//  Created by Lubos Lehota on 25/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppView: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ etnetera-task/etnetera-taskTests/AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  etnetera_taskTests.swift
+//  etnetera-taskTests
+//
+//  Created by Lubos Lehota on 25/07/2025.
+//
+
+import Testing
+@testable import etnetera_task
+
+struct etnetera_taskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
